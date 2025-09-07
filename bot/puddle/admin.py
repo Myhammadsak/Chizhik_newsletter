164efd0bdc2a7c815f462f54028ec40157b34f54
Холмухаммад <143716091+Myhammadsak@users.noreply.github.com>
2025-09-07 14:58:36 +0700

    Delete bot directory

--- bot/puddle/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Groups, Newsletter
-
-admin.site.register(CustomUser)
-admin.site.register(Groups)
-admin.site.register(Newsletter)
--- bot/puddle/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PuddleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'puddle'
-
-    def ready(self):
-        import puddle.signals
